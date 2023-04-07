c76cd2bde82c4d327d5fc9dd6dd50861fe3f602c
Osman Emin Dağeri <123161151+krakanosman@users.noreply.github.com>
2023-04-07 22:13:36 +0300

    düzenlemeler

---
+++ Assets/character controller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Set up variables for player movement
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        // Set up the character controller component
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Move the player based on input
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
+
