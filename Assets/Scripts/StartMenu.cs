7af5b01981baa5ca8e333b3cc6bce47e9b93f932
Rümeysa Özkan <120032650+rumei001@users.noreply.github.com>
2023-04-08 00:51:43 +0300

    StartMenu Yapıldı

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -8,4 +8,8 @@ public class StartMenu : MonoBehaviour
     public void LoadGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
     }
+
+    public void EndGame() {
+        Application.Quit();      
+    }
 }
