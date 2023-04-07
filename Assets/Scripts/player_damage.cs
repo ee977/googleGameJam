3a4e1d62a59662a90c9eaf185f3d1898f37247de
K. Furkan Bayram <furkan.bayram@ozu.edu.tr>
2023-04-08 00:10:51 +0300

    HealthBar

--- Assets/Scripts/player_damage.cs
+++ Assets/Scripts/player_damage.cs
@@ -6,18 +6,17 @@ public class player_damage : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
-    public HealthBar healthBar;
 
     void Start()
     {
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
+        
 
         if (currentHealth <= 0)
         {
