245f1d095706c5fec4b2fff7c08e717da1e0b5eb
Osman Emin Dağeri <123161151+krakanosman@users.noreply.github.com>
2023-04-07 22:20:59 +0300

    enemy movement eklendi

---
+++ Assets/Scripts/enemy game object.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Set up variables for enemy movement and behavior
+    public float speed = 3.0f;
+    public Transform player;
+
+    void Start()
+    {
+        // Set up the player transform reference
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        // Move the enemy towards the player
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the enemy collides with the player, apply damage
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().TakeDamage();
+        }
+    }
+}
