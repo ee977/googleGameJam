431201eaa0224abf0bea7da5f4af7f54f0ad91d7
Osman Emin Dağeri <123161151+krakanosman@users.noreply.github.com>
2023-04-08 21:38:11 +0300

    bullet

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,53 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public GameObject bulletPrefab;
-    public float fireRate = 0.5f;
-    public AudioClip shootSound;
-    
-    private Rigidbody2D rb;
-    private Vector2 movement;
-    private float nextFireTime = 0f;
-    
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
-        if (Input.GetButton("Fire1") && Time.time >= nextFireTime)
-        {
-            Shoot();
-            nextFireTime = Time.time + fireRate;
-        }
-    }
-    
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
-    }
-    
-    void Shoot()
-    {
-        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-        AudioSource.PlayClipAtPoint(shootSound, transform.position);
+   public Transform firePoint;
+   public GameObject bulletPrefab;
 
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
-        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
-        bulletRB.velocity = direction * 10f;
+   public float bulletForce = 20f;
 
-        // Ignore collisions with ground or other objects
-        Physics2D.IgnoreLayerCollision(bullet.layer, LayerMask.NameToLayer("Ground"), true);
-        Physics2D.IgnoreLayerCollision(bullet.layer, LayerMask.NameToLayer("Default"), true);
-    }
+   private void Update()
+   {
+      if (Input.GetButtonDown("Fire1"))
+      {
+         Shoot();
+      }
+   }
 
-}
+   void Shoot()
+   {
+      GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+      Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+      rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+   }
+}    
+  
