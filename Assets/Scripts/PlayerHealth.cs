2086c60773203bd648290767089bae834af28242
K. Furkan Bayram <furkan.bayram@ozu.edu.tr>
2023-04-09 00:40:59 +0300

    Can

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,57 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ClearSky;
+using TMPro;
+
 
 
 
     public class PlayerHealth : MonoBehaviour
-    {
-        [SerializeField] private float health = 100;
-        private Animator anim;
-
-        private int MAX_HEALTH = 100;
-        
-        void Awake()
         {
-            anim = GetComponent<Animator>();
-        }
-   
-        void Update(){
-            if (Input.GetKeyDown(KeyCode.H)){
-                Heal(10);
+            [SerializeField] private float health = 100;
+            private Animator anim;
+            [SerializeField]private TMP_Text text;
+    
+            private int MAX_HEALTH = 100;
+            
+            void Awake()
+            {
+                anim = GetComponent<Animator>();
+                text.text = "" + health;
+            }
+       
+            void Update(){
+                if (Input.GetKeyDown(KeyCode.Mouse0)){
+                    Damage(10);
+                }
+                if (Input.GetKeyDown(KeyCode.H)){
+                    Heal(10);
+                }
+                
+            }
+    
+            public void Damage(float amount){
+                if(amount < 0){
+                    throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+                }
+    
+                this.health -= amount;
+                text.text = "" + health;
+    
+                if(health <= 0){
+                    Die();
+                    text.text = "0";
+                }
+            }
+    
+            public void Heal(int amount){
+                if (amount < 0){
+                    throw new System.ArgumentOutOfRangeException("Cannot have negative healing");
+                }
+    
+                bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+    
+                if (wouldBeOverMaxHealth){
+                    this.health = MAX_HEALTH;
+                }else{
+                    this.health += amount;
+                }
+            }
+    
+            private void Die()
+            {
+                Debug.Log("I am Dead!");
+                anim.SetTrigger("die");
             }
         }
-
-        public void Damage(float amount){
-            if(amount < 0){
-                throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
-            }
-
-            this.health -= amount;
-
-            if(health <= 0){
-                Die();
-            }
-        }
-
-        public void Heal(int amount){
-            if (amount < 0){
-                throw new System.ArgumentOutOfRangeException("Cannot have negative healing");
-            }
-
-            bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
-
-            if (wouldBeOverMaxHealth){
-                this.health = MAX_HEALTH;
-            }else{
-                this.health += amount;
-            }
-        }
-
-        private void Die()
-        {
-            Debug.Log("I am Dead!");
-            anim.SetTrigger("die");
-        }
-    }
-
